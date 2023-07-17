73af9e1b1b7e45398e07e37a84279e6f5d563ff9
lebnaa <lebnanoori@gmail.com>
2023-07-17 11:18:54 -0400

    update

--- Workshops/w7p1/Truck .h
+++ Workshops/w7p1/Truck .h
@@ -13,3 +13,29 @@ piece of work is entirely of my own creation.
 *****************************************************************************/
 #ifndef SDDS_TRUCK_H
 #define SDDS_TRUCK_H
+
+#include <iostream>
+#include "MotorVehicle .h"
+namespace sdds {
+
+	class Truck : public MotorVehicle 
+	{
+		double MaxCapacity{ 0.00 }; 
+		double CurrCapacity{ 0.00 };
+	public:
+		Truck() {};
+		Truck(const char* _license, int _year, double _capacity, const char* address = nullptr); 
+		~Truck() {}; 
+
+		//adds to the attribute that stores the current cargo load the
+		//weight specified as a parameter
+		bool addCargo(double cargo); 
+
+		//unloads current cargo (sets the attribute to 0).
+		bool unloadCargo();
+	};
+
+	std::ostream& operator<<(std::ostream& ostr, const Truck& src);
+	std::istream& operator>>(std::istream& istr, Truck& src);
+
+}
